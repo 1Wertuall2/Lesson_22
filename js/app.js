9674fb0c5028ba9095ebe96b3f7cc9b64df02083
1Wertuall2 <112894532+1Wertuall2@users.noreply.github.com>
2022-12-30 21:32:08 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,38 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    window["FLS"] = true;
+    isWebp();
+    console.log("Учим JS");
+    let userEyesColor = "#9cf2c5";
+    console.log(userEyesColor);
+    if (true) var size = 15;
+    console.log(size);
+    let userHeight = 145 / 0;
+    console.log(userHeight);
+    let userName;
+    console.log(typeof userName);
+    let userSize = "45" / "8";
+    console.log(typeof userSize);
+})();
